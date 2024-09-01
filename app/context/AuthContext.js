dc94108a8e79aa2ef295793f72786b051a64d601
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-09-01 02:41:56 -0700

    Sign in feature implemented using firebase and firebase's GoogleAuthProvider. Created AuthProvider to listen to user state changed.

---
+++ app/context/AuthContext.js
@@ -0,0 +1,33 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '@/firebase'; 
+import { onAuthStateChanged } from 'firebase/auth';
+
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false);
+        });
+
+        // Cleanup subscription on component unmount
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+};
+
+export const useAuth = () => useContext(AuthContext);
--- app/page.js
+++ app/page.js
@@ -1,11 +1,42 @@
+'use client';
 import Image from "next/image";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useAuth } from "./context/AuthContext";
+import { auth } from "@/firebase";
+import { useEffect } from "react";
+
 
 export default function Home() {
+  const { user, loading } = useAuth(); // Use the context to access and loading state
+
+  
+  const handleGoogleSignIn = async (e) => {
+    const provider =  new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+      // The AuthContext will automatically update because of the onAuthStateChanged listener
+    } catch (error) {
+      console.error("Error during sign-in: ", error);
+      // Optionally, handle errors such as showing an error message to the user
+    }
+  };
+
+  useEffect(() => {
+    if(!loading && user) {
+      console.log("User is signed in: ", user);
+    }
+  }, [loading, user]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
         Welcome to ProJourney
       </h1>
+      <button
+        onClick={handleGoogleSignIn}
+      >
+        Sign In with Google
+      </button>
     </main>
   );
 }
