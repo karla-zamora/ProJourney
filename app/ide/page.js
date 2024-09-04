3e60600409d3b93082f627b04e0be66a2312931a
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-09-03 19:24:00 -0700

    Added better ide layout and design just for testing compilation.

--- app/ide/page.js
+++ app/ide/page.js
@@ -1,9 +1,14 @@
 "use client";
 import { useState } from "react";
+import MonacoEditorComponent from "@/components/MonacoEditorComponent";
 
 export default function Page() {
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState("# Write your code here");
   const [output, setOutput] = useState("");
+
+  const handleEditorChange = (value) => {
+    setCode(value);
+  };
 
   const handleRunCode = async () => {
     try {
@@ -31,16 +36,47 @@ export default function Page() {
   };
 
   return (
-    <div>
-      <h1>CodeEditor</h1>
-      <textarea
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-        placeholder="Type your code here..."
-        className="bg-slate-400 "
-      />
-      <button onClick={handleRunCode}>Run</button>
-      <pre>{output}</pre>
+    <div className="grid grid-cols-2 gap-25 h-svh p-20">
+      <div className="p-1">
+        <div className="h-1/2 bg-cyan-950 text-white">
+          <h1 className="text-2xl">Problem: Name</h1>
+          <p>Description: Description details here</p>
+          <p>Input: someInput</p>
+          <p>Output</p>
+          <p>Sample Input</p>
+          <p>Sample Output</p>
+        </div>
+        <div className="h-1/2">
+          <h1>CodeEditor</h1>
+          <MonacoEditorComponent
+            value={code}
+            language="python"
+            onChange={handleEditorChange}
+          />
+          <button
+            className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full"
+            onClick={handleRunCode}
+          >
+            Run
+          </button>
+        </div>
+      </div>
+      <div className="p-1">
+        <div className="h-1/2 bg-cyan-950 text-white">
+          <h1 className="text-2xl">Output</h1>
+          <pre className="overflow-auto h-full">{output}</pre>
+        </div>
+        <div className="h-1/2">
+          <h1>Test Cases</h1>
+          <div className="bg-cyan-950 text-white">
+            <p>Test Case 1</p>
+            <p>Test Case 2</p>
+            <p>Test Case 3</p>
+            <p>Test Case 4</p>
+            <p>Test Case 5</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
